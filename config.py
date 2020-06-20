adb3d285ab0b7346b2e5c49253a1997af433857c
nash-lian <64564513+nash-lian@users.noreply.github.com>
2020-06-19 17:09:08 -0700

    remove the LUIS app id and access id

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class DefaultConfig:
     PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
-    LUIS_APP_ID = os.environ.get("LuisAppId", "012d3a27-0534-45e6-af6d-48916e684a49")
-    LUIS_API_KEY = os.environ.get("LuisAPIKey", "d3ea37866e40429ca4cdceacbaa60704")
+    LUIS_APP_ID = os.environ.get("LuisAppId", "")
+    LUIS_API_KEY = os.environ.get("LuisAPIKey", "")
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
-    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "westus.api.cognitive.microsoft.com")
+    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "")
